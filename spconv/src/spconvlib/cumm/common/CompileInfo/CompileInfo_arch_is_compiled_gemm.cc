a56f32052bba63c90cb1d4452789593284185930
Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>
2025-10-24 16:47:36 +0900

    feat: add all arch
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>

--- spconv/src/spconvlib/cumm/common/CompileInfo/CompileInfo_arch_is_compiled_gemm.cc
+++ spconv/src/spconvlib/cumm/common/CompileInfo/CompileInfo_arch_is_compiled_gemm.cc
@@ -14,7 +14,22 @@ bool CompileInfo::arch_is_compiled_gemm(std::tuple<int, int> arch)   {
   if (arch == std::make_tuple(8, 6)){
       return true;
   }
+  if (arch == std::make_tuple(8, 7)){
+      return true;
+  }
   if (arch == std::make_tuple(8, 9)){
+      return true;
+  }
+  if (arch == std::make_tuple(9, 0)){
+      return true;
+  }
+  if (arch == std::make_tuple(10, 0)){
+      return true;
+  }
+  if (arch == std::make_tuple(10, 3)){
+      return true;
+  }
+  if (arch == std::make_tuple(11, 0)){
       return true;
   }
   if (arch == std::make_tuple(12, 0)){
